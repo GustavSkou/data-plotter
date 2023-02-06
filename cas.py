c09deaad52f8368a34b447ecc3a40eca67bafcad
GustavSkou <55804968+GustavSkou@users.noreply.github.com>
2023-02-06 17:33:27 +0100

    Add files via upload

--- cas.py
+++ cas.py
@@ -2,8 +2,10 @@ from sympy import *
 import matplotlib.pyplot as plt
 import numpy as np
 
+info_listen = []
+
 x = Symbol("x")
-function = 2*x**2
+function = x**2+x
 
 f = function
 f_mark = f.diff(x)
@@ -21,29 +23,61 @@ print(f_str.replace("**", "^"))
 print(f_mark_str.replace("**", "^"))
 print(F_str.replace("**", "^"))
 
-x_value = np.linspace(-30, 30, 100)
 
-f_xpoints = x_value
-F_xpoints = x_value
-f_mark_xpoints = x_value
+x_akse_info = [-4, 4, 1000]
+
+x_akse = np.linspace(x_akse_info[0], x_akse_info[1], x_akse_info[2])
+
+f_xpoints = x_akse
+F_xpoints = x_akse
+f_mark_xpoints = x_akse
 
 f_ypoints = []
 F_ypoints = []
 f_mark_ypoints = []
+y_akse_info = []
 
 n=0
-for n in range(100):
-    f_ypoints.append(f(x_value[n]))
-    F_ypoints.append(F(x_value[n]))
-    f_mark_ypoints.append(f_mark(x_value[n]))
+for n in range(x_akse_info[2]):
+    f_ypoints.append(f(x_akse[n]))
+    F_ypoints.append(F(x_akse[n]))
+    f_mark_ypoints.append(f_mark(x_akse[n]))
+
+info_listen.extend(f_ypoints)
+info_listen.extend(F_ypoints)
+info_listen.extend(f_mark_ypoints)
+y_akse_info.append(min(info_listen))
+y_akse_info.append(max(info_listen))
 
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
 
-f_graf, = plt.plot(f_xpoints, f_ypoints, label="f(x)")
-#F_graf, = plt.plot(F_xpoints, F_ypoints, label="F(x)")
-f_mark_graf, = plt.plot(f_mark_xpoints, f_mark_ypoints, label="f'(x)")
+k = [0, 0]
+
+f_graf, = plt.plot(f_xpoints, f_ypoints, label="f(x) = "+ f_str.replace("**", "^"))
+F_graf, = plt.plot(F_xpoints, F_ypoints, label="F(x) = " + F_str.replace("**", "^"))
+f_mark_graf, = plt.plot(f_mark_xpoints, f_mark_ypoints, label="f'(x) = "+ f_mark_str.replace("**", "^"))
+
+x_akse_graf, = plt.plot(x_akse_info[0:2], k, linestyle = 'dotted', color="black")
+y_akse_graf, = plt.plot(k, y_akse_info[0:2], linestyle = 'dotted', color="black")
 
 leg = plt.legend(loc='upper left')
 
+plt.ion()
 plt.show()
+
+status = 1
+while status != 0:
+    x_value = float(input("x = "))
+
+    print("f("+str(x_value)+") = " + str(f(x_value)))
+    print("f'("+str(x_value)+") = " + str(f_mark(x_value)))
+    print("F("+str(x_value)+") = " + str(F(x_value)))
+
+    plt.plot(x_value, f(x_value), marker = 'o')
+    plt.plot(x_value, F(x_value), marker = 'o')
+    plt.plot(x_value, f_mark(x_value), marker = 'o')
+
+    plt.annotate(text=str(f(x_value)), xy=(x_value, f(x_value)))
+    plt.annotate(text=str(F(x_value)), xy=(x_value, F(x_value)))
+    plt.annotate(text=str(f_mark(x_value)), xy=(x_value, f_mark(x_value)))
